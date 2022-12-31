396f109c6cfc19b8ff27451373b8dbd611b7daa4
harsha-ys <hyoshith@gmail.com>
2023-01-01 01:33:54 +0530

    factory class and testing class

---
+++ C_factory design pattern/Enemyshipfactory.java
@@ -0,0 +1,7 @@
+public class Enemyshipfactory 
+{
+    public Enemyship makeenemyship(String enemyshiptype)
+    {
+        return ( enemyshiptype.equals("U"))? new UFOenemyship() : ( enemyshiptype.equals("R")? new ROCKETenemyship() :( enemyshiptype.equals("S")? new SUPERUFOenemyship() : null) )
+    }    
+}
---
+++ C_factory design pattern/testing.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class testing 
+{
+    public static void dofight(Enemyship tobefought)
+    {
+        tobefought.displayenemyship();
+        tobefought.followheroship();
+        tobefought.enemyshipshoots();
+    }
+
+    public static void main(String[] args) 
+    {
+        //construct a factory  
+        Enemyshipfactory  shipfactory = new Enemyshipfactory(); //only can use final or without modifierrs inside the methods in java 
+        //only allow local variables insode a method in java 
+        Enemyship tobetested = null;
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("what type of enemy ship do you want : ( U / R / S) ");
+
+
+        if ( input.hasNextLine()) 
+        {
+            String enemyshiptype = input.nextLine();
+            tobetested = shipfactory.makeenemyship(enemyshiptype);
+            if ( tobetested != null) dofight(tobetested); /* we only can call static methods inside another static method
+            so we want to make dofight as static or else create a object of a testing class and call the dofight method from that */
+            else System.out.println("correctly give the enemy ship type");
+
+.        }
+    }
+}
