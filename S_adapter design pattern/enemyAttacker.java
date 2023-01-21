d6404f809b49aa5477a984190fca51cc8fb4a843
harsha-ys <hyoshith@gmail.com>
2023-01-21 11:04:45 +0530

    target interface enemyattacker

---
+++ S_adapter design pattern/enemyAttacker.java
@@ -0,0 +1,7 @@
+//this is the target interface
+public interface enemyAttacker 
+{
+    public void fireWeapon();
+    public void driveForward();
+    public void assignDriver(String driverName);
+}
